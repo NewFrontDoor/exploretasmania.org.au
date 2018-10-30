4091a8e432838814e45e192f072aa322d2e14d83
Bratalix <bratalix@gmail.com>
2018-10-31 10:43:19 +1100

    Wrap trip image in a tag

--- src/components/models/UpcomingTrip.js
+++ src/components/models/UpcomingTrip.js
@@ -13,7 +13,7 @@ class UpcomingTrip extends Component {
             <section>
                 <div className="header"> <h5>{this.props.name}</h5> </div>
                 <div className="content">
-                    <TripPopup trigger={<img className="img img-responsive" src={this.props.img} />} difficulty={this.props.difficulty} /><br />
+                    <TripPopup trigger={<a href="javascript:void(0);"><img className="img img-responsive" src={this.props.img} difficulty={this.props.difficulty} /></a>} /><br />
                     <span style={{ fontSize: "10px" }}>Click on the image for more detailed trip information</span><br /><br />
                     {this.props.desc} <br /><br />
                     <ul className="no-list-style no-margin-or-padding">
