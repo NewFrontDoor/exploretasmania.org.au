bec228012ad42f6faacccb3d3664284ae0e886d7
Bratalix <bratalix@gmail.com>
2018-12-20 15:17:10 +1100

    Remove past trip

--- src/components/pages/Trips/Trips.js
+++ src/components/pages/Trips/Trips.js
@@ -4,9 +4,7 @@ import UpcomingTrip from '../../models/UpcomingTrip';
 
 import image1 from '../../../assets/image1.jpg';
 import image2 from '../../../assets/image2.jpg';
-import RhonaImg from '../../../assets/RhonaTrip.jpg';
 import FreycinetImg from '../../../assets/FreycinetTrip.jpg'
-import LakeRhonaTrip from './LakeRhonaTrip';
 import ThreeDaysAtFreycinet2019 from './ThreeDaysAtFreycinet2019';
 import FourDaysAtFreycinet2019 from './FourDaysAtFreycinet2019';
 
@@ -71,20 +69,6 @@ class Events extends Component {
                                 <br />
                                 <div className="row">
                                   <div className="col-md-6">
-                                    <UpcomingTrip name="Lake Rhona: 13-16 December 2018"
-                                      desc="Lake Rhona is a beautiful lake nestled in the heart of South-West Tasmania. It has a spectacular white sand beach and is overlooked by Reeds Peak, a rugged crag on the Dennison Range. This unique part of Tasmania’s wilderness is a place you will never forget."
-                                      img={RhonaImg}
-                                      difficulty={4}
-                                      activity="Bushwalking"
-                                      length="4 days"
-                                      popupContent={<LakeRhonaTrip />}
-                                    />
-                                  </div>
-                                </div>
-                                <br />
-                                <div className="clearfix"></div>
-                                <div className="row">
-                                  <div className="col-md-6">
                                     <UpcomingTrip name="3 days at Freycinet: 11-13 January 2019"
                                       desc="Freycinet is described by guide books as having some of the most beautiful beaches in the world. The sand is white, the ocean is pristine blue, the sea cliffs and spectacular and the surrounding mountains just add to the wonder. On this 3 day trip plan to rock climb and abseil on the sea cliffs, walk up one of the mountains and visit two of the world-renowned beaches."
                                       img={FreycinetImg}
@@ -94,7 +78,10 @@ class Events extends Component {
                                       popupContent={<ThreeDaysAtFreycinet2019 />}
                                     />
                                   </div>
-
+                                </div>
+                                <br />
+                                <div className="clearfix"></div>
+                                <div className="row">
                                   <div className="col-md-6">
                                     <UpcomingTrip name="4 days at Freycinet: 17-20 January 2019"
                                       desc="Freycinet is described by guide books as having some of the most beautiful beaches in the world. The sand is white, the ocean is pristine blue, the sea cliffs and spectacular and the surrounding mountains just add to the wonder. On this 4 day trip we plan to rock climb and abseil on the sea cliffs, walk up one of the mountains and visit four of the world-renowned beaches."
